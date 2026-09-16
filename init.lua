b2613d415ac7d8fd3288f492b473621d8fbfa5ea
Sheriff_U3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-09-15 20:42:11 -0500

    Remove dofile call

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ local modpath = core.get_modpath(core.get_current_modname())
 ccf.MODNAME = core.get_current_modname()
 ccf.MODPATH = core.get_modpath(core.get_current_modname())
 
-dofile(modpath .. "/settings.lua")
 dofile(modpath .. "/groupimg.lua")
 dofile(modpath .. "/logic.lua")
 dofile(modpath .. "/ui.lua")
